4142cce2e6dd41e4d58b9820ccbd17f7855c7555
Leonardo <leo_narita@hotmail.com>
2021-04-29 21:13:53 -0300

    Aplicação dos conceitos

--- EXAMPLE03/src/app/app.component.ts
+++ EXAMPLE03/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as fileSaver from 'file-saver'
+import { UploadFileService } from './apps/services/upload-file.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EXAMPLE03';
+
+  items = [20, 30, 40]
+
+  file: File;
+
+  constructor(private uploadService: UploadFileService) {
+  }
+
+  handleFile(files: FileList) {
+    this.file = files.item(0);
+  }
+
+  upload() {
+    this.uploadService.upload(this.file).subscribe(data => {
+      console.log(data)
+      this.download(data, this.file.name)
+    })
+  }
+
+  download(data: Blob, filename: string) {
+    fileSaver.saveAs(data, filename)
+  }
+
+  downloadHelloWorld() {
+    var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+    this.download(blob, 'teste.txt')
+  }
+
 }
